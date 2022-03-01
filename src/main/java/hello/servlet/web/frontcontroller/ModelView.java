bae9d86b16cc27c15c9282271a3a4c8a3374342b
kyupid <iamscvv@gmail.com>
2022-03-01 12:30:47 +0900

    [#2] HttpServeletRequest, Response가 없는 내가 만든 프레임워크에 종속적인것, 서블릿에 종속적이진 않음

---
+++ src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -0,0 +1,25 @@
+package hello.servlet.web.frontcontroller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModelView {
+    private String viewName;
+    private Map<String, Object> model = new HashMap<>();
+
+    public String getViewName() {
+        return viewName;
+    }
+
+    public Map<String, Object> getModel() {
+        return model;
+    }
+
+    public void setViewName(String viewName) {
+        this.viewName = viewName;
+    }
+
+    public void setModel(Map<String, Object> model) {
+        this.model = model;
+    }
+}
