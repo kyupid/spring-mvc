9911380ed560b5ce8d67388250f14b47e65af144
kyupid <iamscvv@gmail.com>
2022-03-01 12:40:51 +0900

    [#2] Model추가를 위한 ModelView 객체를 반환하도록 기존 컨트롤러들 모두 수정

--- src/main/java/hello/servlet/web/frontcontroller/ModelView.java
+++ src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -7,6 +7,10 @@ public class ModelView {
     private String viewName;
     private Map<String, Object> model = new HashMap<>();
 
+    public ModelView(String viewName) {
+        this.viewName = viewName;
+    }
+
     public String getViewName() {
         return viewName;
     }
